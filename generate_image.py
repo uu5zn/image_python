846cca80f1bf731a55cf43d74802d325f65abb84
uu5zn <uu5zn@users.noreply.github.com>
2025-01-22 05:42:14 +0000

    deploy: 08e07cdcd2507752b70b312e3ef5bfe26db71d87

---
+++ generate_image.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# 你的代码在这里
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 生成一些随机数据
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# 创建图表
+plt.figure(figsize=(8, 6))
+plt.plot(x, y)
+plt.title("Generated Image")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+# 保存图片
+plt.savefig("image.png") # 将图片保存为 image.png
